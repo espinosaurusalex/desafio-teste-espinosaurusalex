463eb04b5d2d5e0c1ba5050a738e91183c3adccb
Gian Lucas Lima <mashibadanthelion@hotmail.com>
2025-09-09 17:13:21 -0300

    Add files via upload

---
+++ desafioespinosaurusalex.js
@@ -0,0 +1,27 @@
+
+class AbrigoAnimais{
+    encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais){
+    const BRINQUEDOS_VALIDOS=['RATO', 'BOLA', 'CAIXA', 'NOVELO', 'LASER', 'SKATE' ];
+   }
+   }
+
+   
+
+
+
+
+const Animais = {
+    Rex: {tipo: 'cao', brinquedos :['RATO', 'BOLA'] },
+    Mimi: {tipo: 'gato', brinquedos:['BOLA', 'LASER'] },
+    Fofo: {tipo: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+    Zero: {tipo: 'gato', brinquedos: ['RATO', 'BOLA'] },
+    Bola: {tipo: 'cao', brinquedos: ['CAIXA', 'NOVELO'] },
+    Bebe: {tipo: 'cao', brinquedos:['LASER', 'RATO', 'BOLA'] },
+    Loco: {tipo: 'jabuti', brinquedos: ['SKATE', 'RATO']}
+
+
+}
+
+
+
+
